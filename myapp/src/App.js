901ea678697a7d8757ec30b1f8b109751cab658e
ChamathkaDhanapala <139616499+ChamathkaDhanapala@users.noreply.github.com>
2024-11-19 16:46:11 +0000

    studenttable.js creation

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { students } from './StudentsDb';
+import StudentTable from './components/StudentTable';
+import StudentProfile from './components/StudentProfile';
+import FontSizeChanger from './components/FontSizeChanger';
+import './compo.css';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [students] = useState(StudentsDb);
+  const [selectedStudent, setSelectedStudent] = useState(students[0]);
+  const [fontSize, setFontSize] = useState("16px");
+
+  const selectStudent = (index) => {
+    setSelectedStudent(students[index]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      <h2 className="topic">Students Information Portal</h2>
+      <hr />
+      <FontSizeChanger fontSize={fontSize} setFontSize={setFontSize} />
+      <hr/>
+      <div className="outerDiv" style={{ fontSize }}>
+       
+        <div className="leftDiv">
+          
+          <br />
+          <Table students={students} selectStudent={selectStudent} />
+        </div>
+        <div className="rightDiv">
+          <Profile student={selectedStudent} />
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
